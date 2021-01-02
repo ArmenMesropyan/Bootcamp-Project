fec2596f66f6e235c5816bebf994d5a0d8d75798
ArmenMesropyan <armenbangits@gmail.com>
2021-01-02 12:54:21 +0400

    Added morgan logger in project

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const bootcampRoutes = require('./routes/bootcamps');
+const morgan = require('morgan');
 
 // Load env variables
 dotenv.config({path: './config/config.env'});
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 // Routers
 app.use('/api/v1/bootcamps', bootcampRoutes);
